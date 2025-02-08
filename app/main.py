36aec429a9405c495899110cd8a36901d3a7c49a
sLy23afk <manashbhowmick13@gmail.com>
2025-02-08 23:32:32 +0530

    ok

--- app/main.py
+++ app/main.py
@@ -2,24 +2,24 @@ import sys
 
 
 def main():
- while True   # Uncomment this block to pass the first stage
-  sys.stdout.write("$ ")
+    # Uncomment this block to pass the first stage
+    sys.stdout.write("$ ")
 
     # Wait for user input
+command = input()    
+while True:
+ print(f"{command}: command not found")
+
+#     # Print the prompt
+ 
     
-
-
-
-    # Print the prompt
-command = input("$")
-    
-    # Check if input is empty (user pressed Enter)
-if not command.strip():
-        continue
+#     # Check if input is empty (user pressed Enter)
+#  if not command.strip():
+#         continue
     
     # Print command not found error
-print(f"{command}: command not found")
+   
 
 
-if __name__ == "__main__":
+ if __name__ == "__main__":
     main()
