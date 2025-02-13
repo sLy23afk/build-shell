613cd34a55bf89bfcb7bff030b9dcc6f2620a5b8
sLy23afk <manashbhowmick13@gmail.com>
2025-02-13 11:29:43 +0530

    amama

--- app/main.py
+++ app/main.py
@@ -40,9 +40,9 @@ def main():
         elif word_check not in parts:
         # next = input()
           print(f"{command}: command not found") 
-        elif command.lower().startswith("exit"):
+        if command.lower().startswith("exit"):
             sys.exit(0 if len(command.split()) == 1 
-                     else int(command.split()[1]))
+                         else int(command.split()[1]))
             
        
 
